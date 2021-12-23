4c182827382d670b7836f22fd27aafc02fa222f8
Xavier-Frédéric Moulet <xavier-frederic.moulet@orange.com>
2021-12-23 23:27:50 +0100

    Make QOI encoder/decoder compliant with new format

--- qoi.go
+++ qoi.go
@@ -19,23 +19,22 @@ import (
 )
 
 const (
-	qoi_INDEX   byte = 0b00_000000
-	qoi_RUN_8   byte = 0b010_00000
-	qoi_RUN_16  byte = 0b011_00000
-	qoi_DIFF_8  byte = 0b10_000000
-	qoi_DIFF_16 byte = 0b110_00000
-	qoi_DIFF_24 byte = 0b1110_0000
-	qoi_COLOR   byte = 0b1111_0000
+	qoi_INDEX byte = 0b00_000000
+	qoi_DIFF  byte = 0b01_000000
+	qoi_LUMA  byte = 0b10_000000
+	qoi_RUN   byte = 0b11_000000
+	qoi_RGB   byte = 0b1111_1110
+	qoi_RGBA  byte = 0b1111_1111
 
 	qoi_MASK_2 byte = 0b11_000000
-	qoi_MASK_3 byte = 0b111_00000
-	qoi_MASK_4 byte = 0b1111_0000
 )
 
 const qoiMagic = "qoif"
 
+const qoiPixelsMax = 400_000_000 // 400 million pixels ought to be enough for anybody
+
 func qoi_COLOR_HASH(r, g, b, a byte) byte {
-	return byte(r ^ g ^ b ^ a)
+	return byte(r*3 + g*5 + b*7 + a*11)
 }
 
 type pixel [4]byte
@@ -44,6 +43,10 @@ func Decode(r io.Reader) (image.Image, error) {
 	cfg, err := DecodeConfig(r)
 	if err != nil {
 		return nil, err
+	}
+	NBPixels := cfg.Width * cfg.Height
+	if NBPixels == 0 || NBPixels > qoiPixelsMax {
+		return nil, errors.New("bad image dimensions")
 	}
 
 	b := bufio.NewReader(r)
@@ -60,89 +63,44 @@ func Decode(r io.Reader) (image.Image, error) {
 		if run > 0 {
 			run--
 		} else {
-
 			b1, err := b.ReadByte()
-			// read and handle end of file
 			if err == io.EOF {
 				return img, nil
 			}
 			if err != nil {
 				return nil, err
 			}
+
 			switch {
-			case (b1 & qoi_MASK_2) == qoi_INDEX:
-				px = index[b1^qoi_INDEX]
-
-			case (b1 & qoi_MASK_3) == qoi_RUN_8:
-				run = int(b1 & 0x1f)
-
-			case ((b1 & qoi_MASK_3) == qoi_RUN_16):
-				b2, err := b.ReadByte()
+			case b1 == qoi_RGB:
+				_, err = io.ReadFull(b, px[:3])
 				if err != nil {
 					return nil, err
 				}
-				run = (((int(b1) & 0x1f) << 8) | int(b2)) + 32
-
-			case ((b1 & qoi_MASK_2) == qoi_DIFF_8):
+			case b1 == qoi_RGBA:
+				_, err = io.ReadFull(b, px[:])
+				if err != nil {
+					return nil, err
+				}
+			case b1&qoi_MASK_2 == qoi_INDEX:
+				px = index[b1]
+			case b1&qoi_MASK_2 == qoi_DIFF:
 				px[0] += ((b1 >> 4) & 0x03) - 2
 				px[1] += ((b1 >> 2) & 0x03) - 2
 				px[2] += (b1 & 0x03) - 2
-
-			case ((b1 & qoi_MASK_3) == qoi_DIFF_16):
+			case b1&qoi_MASK_2 == qoi_LUMA:
 				b2, err := b.ReadByte()
 				if err != nil {
 					return nil, err
 				}
-				px[0] += (b1 & 0x1f) - 16
-				px[1] += (b2 >> 4) - 8
-				px[2] += (b2 & 0x0f) - 8
-
-			case ((b1 & qoi_MASK_4) == qoi_DIFF_24):
-				b2, err := b.ReadByte()
-				if err != nil {
-					return nil, err
-				}
-				b3, err := b.ReadByte()
-				if err != nil {
-					return nil, err
-				}
-
-				px[0] += (((b1 & 0x0f) << 1) | (b2 >> 7)) - 16
-				px[1] += ((b2 & 0x7c) >> 2) - 16
-				px[2] += (((b2 & 0x03) << 3) | ((b3 & 0xe0) >> 5)) - 16
-				px[3] += (b3 & 0x1f) - 16
-
-			case (b1 & qoi_MASK_4) == qoi_COLOR:
-				if b1&8 != 0 {
-					b2, err := b.ReadByte()
-					if err != nil {
-						return nil, err
-					}
-					px[0] = b2
-				}
-				if b1&4 != 0 {
-					b2, err := b.ReadByte()
-					if err != nil {
-						return nil, err
-					}
-					px[1] = b2
-				}
-				if b1&2 != 0 {
-					b2, err := b.ReadByte()
-					if err != nil {
-						return nil, err
-					}
-					px[2] = b2
-				}
-				if b1&1 != 0 {
-					b2, err := b.ReadByte()
-					if err != nil {
-						return nil, err
-					}
-					px[3] = b2
-				}
+				vg := (b1 & 0b00111111) - 32
+				px[0] += vg - 8 + ((b2 >> 4) & 0x0f)
+				px[1] += vg
+				px[2] += vg - 8 + (b2 & 0x0f)
+			case b1&qoi_MASK_2 == qoi_RUN:
+				run = int(b1 & 0b00111111)
 			default:
-				px = pixel{255, 0, 255, 255}
+				px = pixel{255, 0, 255, 255} // should not happen
 			}
 
 			index[int(qoi_COLOR_HASH(px[0], px[1], px[2], px[3]))%len(index)] = px
@@ -156,12 +114,18 @@ func Decode(r io.Reader) (image.Image, error) {
 }
 
 func Encode(w io.Writer, m image.Image) error {
+
+	var out = bufio.NewWriter(w)
+
 	minX := m.Bounds().Min.X
 	maxX := m.Bounds().Max.X
 	minY := m.Bounds().Min.Y
 	maxY := m.Bounds().Max.Y
 
-	var out = bufio.NewWriter(w)
+	NBPixels := (maxX - minX) * (maxY - minY)
+	if NBPixels == 0 || NBPixels >= qoiPixelsMax {
+		return errors.New("Bad image Size")
+	}
 
 	// write header to output
 	if err := binary.Write(out, binary.BigEndian, []byte(qoiMagic)); err != nil {
@@ -197,79 +161,46 @@ func Encode(w io.Writer, m image.Image) error {
 
 			if px == px_prev {
 				run++
-			}
-
-			last_pixel := x == (maxX-1) && y == (maxY-1)
-			if run > 0 && (run == 0x2020 || px != px_prev || last_pixel) {
-				if run < 33 {
-					out.WriteByte(qoi_RUN_8 | byte(run-1))
-				} else {
-					run -= 33
-					out.WriteByte(qoi_RUN_16 | byte(run>>8))
-					out.WriteByte(byte(run & 0xff))
+				last_pixel := x == (maxX-1) && y == (maxY-1)
+				if run == 62 || last_pixel {
+					out.WriteByte(qoi_RUN | byte(run-1))
+					run = 0
 				}
-				run = 0
-			}
-
-			if px != px_prev {
+			} else {
+				if run > 0 {
+					out.WriteByte(qoi_RUN | byte(run-1))
+					run = 0
+				}
 				var index_pos byte = qoi_COLOR_HASH(px[0], px[1], px[2], px[3]) % 64
 				if index[index_pos] == px {
 					out.WriteByte(qoi_INDEX | index_pos)
 				} else {
 					index[index_pos] = px
 
-					vr := int(px[0]) - int(px_prev[0])
-					vg := int(px[1]) - int(px_prev[1])
-					vb := int(px[2]) - int(px_prev[2])
-					va := int(px[3]) - int(px_prev[3])
+					if px[3] == px_prev[3] {
+						vr := int8(int(px[0]) - int(px_prev[0]))
+						vg := int8(int(px[1]) - int(px_prev[1]))
+						vb := int8(int(px[2]) - int(px_prev[2]))
 
-					if vr > -17 && vr < 16 &&
-						vg > -17 && vg < 16 &&
-						vb > -17 && vb < 16 &&
-						va > -17 && va < 16 {
-						switch {
-						case va == 0 &&
-							vr > -3 && vr < 2 &&
-							vg > -3 && vg < 2 &&
-							vb > -3 && vb < 2:
-							out.WriteByte(qoi_DIFF_8 | byte(((vr+2)<<4)|(vg+2)<<2|(vb+2)))
-						case va == 0 &&
-							vr > -17 && vr < 16 &&
-							vg > -9 && vg < 8 &&
-							vb > -9 && vb < 8:
-							out.WriteByte(qoi_DIFF_16 | byte(vr+16))
-							out.WriteByte(byte(((vg + 8) << 4) | (vb + 8)))
-						default:
-							out.WriteByte(qoi_DIFF_24 | byte((vr+16)>>1))
-							out.WriteByte(byte(((vr + 16) << 7) | ((vg + 16) << 2) | ((vb + 16) >> 3)))
-							out.WriteByte(byte(((vb + 16) << 5) | (va + 16)))
-						}
-					} else {
-						mask := qoi_COLOR
-						if vr != 0 {
-							mask |= 1 << 3
-						}
-						if vg != 0 {
-							mask |= 1 << 2
-						}
-						if vb != 0 {
-							mask |= 1 << 1
-						}
-						if va != 0 {
-							mask |= 1 << 0
-						}
-						out.WriteByte(mask)
-						if vr != 0 {
+						vg_r := vr - vg
+						vg_b := vb - vg
+
+						if vr > -3 && vr < 2 && vg > -3 && vg < 2 && vb > -3 && vb < 2 {
+							out.WriteByte(qoi_DIFF | byte((vr+2)<<4|(vg+2)<<2|(vb+2)))
+						} else if vg_r > -9 && vg_r < 8 && vg > -33 && vg < 32 && vg_b > -9 && vg_b < 8 {
+							out.WriteByte(qoi_LUMA | byte(vg+32))
+							out.WriteByte(byte((vg_r+8)<<4) | byte(vg_b+8))
+						} else {
+							out.WriteByte(qoi_RGB)
 							out.WriteByte(px[0])
-						}
-						if vg != 0 {
 							out.WriteByte(px[1])
-						}
-						if vb != 0 {
 							out.WriteByte(px[2])
 						}
-						if va != 0 {
-							out.WriteByte(px[3])
+
+					} else {
+						out.WriteByte(qoi_RGBA)
+						for i := 0; i < 4; i++ {
+							out.WriteByte(px[i])
 						}
 					}
 
@@ -280,6 +211,7 @@ func Encode(w io.Writer, m image.Image) error {
 		}
 	}
 	binary.Write(out, binary.BigEndian, uint32(0)) // padding
+	binary.Write(out, binary.BigEndian, uint32(1)) // padding
 
 	return out.Flush()
 }
@@ -293,7 +225,7 @@ func DecodeConfig(r io.Reader) (cfg image.Config, err error) {
 	if string(header[:4]) != qoiMagic {
 		return cfg, errors.New("Invalid magic")
 	}
-
+	// only decodes as NRGBA images
 	return image.Config{
 		Width:      int(binary.BigEndian.Uint32(header[4:])),
 		Height:     int(binary.BigEndian.Uint32(header[8:])),
